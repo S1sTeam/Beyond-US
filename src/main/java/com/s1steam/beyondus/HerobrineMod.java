4bc5cd0eb4c9951450f8d7ea5cd08e8c23528d13
S1sTeam <211373039+S1sTeam@users.noreply.github.com>
2025-08-02 02:53:11 +0700

    Update HerobrineMod.java

--- src/main/java/com/s1steam/beyondus/HerobrineMod.java
+++ src/main/java/com/s1steam/beyondus/HerobrineMod.java
@@ -8,6 +8,19 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = HerobrineMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ClientModEvents {
+
+    @SubscribeEvent
+    public static void onRegisterLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(HerobrineModel.LAYER_LOCATION, HerobrineModel::createBodyLayer);
+    }
+}
+
 
 @Mod(HerobrineMod.MODID)
 public class HerobrineMod {
