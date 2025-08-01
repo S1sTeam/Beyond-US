03dcad0fddba054143c61d0b8da2f1f29f130892
S1sTeam <211373039+S1sTeam@users.noreply.github.com>
2025-08-02 02:45:55 +0700

    Update HerobrineMod.java

--- src/main/java/com/s1steam/beyondus/HerobrineMod.java
+++ src/main/java/com/s1steam/beyondus/HerobrineMod.java
@@ -1,12 +1,15 @@
 package com.s1steam.beyondus;
 
 import com.s1steam.beyondus.entity.EntityHerobrine;
+import com.s1steam.beyondus.entity.HerobrineSpawning;
 import com.s1steam.beyondus.registry.MySounds;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 
 @Mod(HerobrineMod.MODID)
 public class HerobrineMod {
@@ -15,15 +18,26 @@ public class HerobrineMod {
     public HerobrineMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Регистрация сущностей
+        // Регистрация сущности
         EntityHerobrine.register(modEventBus);
 
         // Регистрация звуков
         MySounds.register(modEventBus);
 
-        // Клиентские события: регистрируем рендеры
+        // Регистрация спауна
+        HerobrineSpawning.registerSpawnPlacement();
+
+        // Регистрируем рендер на клиенте
         if (FMLEnvironment.dist == Dist.CLIENT) {
-            modEventBus.register(ClientEventHandler.class);
+            modEventBus.addListener(this::registerEntityRenderers);
         }
+
+        // Регистрируем события спауна
+        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(HerobrineSpawning.class);
+    }
+
+    private void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(EntityHerobrine.HEROBRINE.get(), context -> new EntityHerobrine.Render(context));
     }
 }
