ba5c7040c4fa9d3d77304586382a2896612d01da
S1sTeam <211373039+S1sTeam@users.noreply.github.com>
2025-08-02 02:14:25 +0700

    Update EntityHerobrine.java

--- src/main/java/com/s1steam/beyondus/entity/EntityHerobrine.java
+++ src/main/java/com/s1steam/beyondus/entity/EntityHerobrine.java
@@ -25,6 +25,8 @@ import net.minecraftforge.network.NetworkHooks;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.entity.EntityType;
+
 
 public class EntityHerobrine extends Monster {
 
