aea52c89dc672304cc6d2e79fdf695519bc0083d
S1sTeam <211373039+S1sTeam@users.noreply.github.com>
2025-08-02 02:19:45 +0700

    Update HerobrineMod.java

--- src/main/java/com/s1steam/beyondus/HerobrineMod.java
+++ src/main/java/com/s1steam/beyondus/HerobrineMod.java
@@ -2,13 +2,13 @@ package com.s1steam.beyondus;
 
 import com.s1steam.beyondus.entity.EntityHerobrine;
 import com.s1steam.beyondus.registry.MySounds;
-import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 
 @Mod(HerobrineMod.MODID)
 public class HerobrineMod {
@@ -17,21 +17,26 @@ public class HerobrineMod {
     public HerobrineMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Регистрируем сущность
+        // Регистрируем сущности и звуки
         EntityHerobrine.register(modEventBus);
-
-        // Регистрируем звуки
         MySounds.register(modEventBus);
 
-        // Регистрируем рендер только на клиенте
+        // Регистрируем рендеры и модели только на клиенте
         if (FMLEnvironment.dist == Dist.CLIENT) {
             modEventBus.addListener(this::registerEntityRenderers);
+            modEventBus.addListener(this::registerLayerDefinitions);
         }
 
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void registerEntityRenderers(final net.minecraftforge.client.event.EntityRenderersEvent.RegisterRenderers event) {
-        event.registerEntityRenderer(EntityHerobrine.HEROBRINE.get(), context -> new EntityHerobrine.Render(context));
+    // Регистрация рендера сущности
+    private void registerEntityRenderers(final EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(EntityHerobrine.HEROBRINE.get(), EntityHerobrine.Render::new);
+    }
+
+    // Регистрация слоя модели для правильной работы bakeLayer
+    private void registerLayerDefinitions(final EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(HerobrineModel.LAYER_LOCATION, HerobrineModel::createBodyLayer);
     }
 }
