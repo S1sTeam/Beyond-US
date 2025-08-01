4c4c43b8b7c1f011c784c6513252560f751a7b02
S1sTeam <211373039+S1sTeam@users.noreply.github.com>
2025-08-02 02:30:37 +0700

    Update HerobrineModel.java

--- src/main/java/com/s1steam/beyondus/entity/HerobrineModel.java
+++ src/main/java/com/s1steam/beyondus/entity/HerobrineModel.java
@@ -2,18 +2,18 @@ package com.s1steam.beyondus.entity;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.builders.CubeDeformation;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.model.geom.builders.MeshDefinition;
+
 import net.minecraft.resources.ResourceLocation;
 
 public class HerobrineModel extends HumanoidModel<EntityHerobrine> {
-
-    public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(
-            new ResourceLocation("beyondus", "herobrine"), "main");
+    public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("beyondus", "herobrine"), "main");
 
     public HerobrineModel(ModelPart root) {
         super(root);
@@ -27,12 +27,12 @@ public class HerobrineModel extends HumanoidModel<EntityHerobrine> {
     @Override
     public void setupAnim(EntityHerobrine entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
         super.setupAnim(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
-        // Можно добавить кастомные анимации
+        // Добавьте кастомные анимации здесь, если нужно
     }
 
     @Override
-    public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer,
-                               int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+    public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay,
+                               float red, float green, float blue, float alpha) {
         super.renderToBuffer(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
     }
 }
