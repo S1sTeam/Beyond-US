ed1951ef85e0e2ded17c17b23c5323b1dd769019
S1sTeam <211373039+S1sTeam@users.noreply.github.com>
2025-08-02 01:29:18 +0700

    Update HerobrineModel.java

--- src/main/java/com/s1steam/beyondus/entity/HerobrineModel.java
+++ src/main/java/com/s1steam/beyondus/entity/HerobrineModel.java
@@ -3,25 +3,65 @@ package com.s1steam.beyondus.entity;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.model.geom.LayerDefinition;
-import net.minecraft.client.model.geom.builders.CubeDeformation;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.model.geom.builders.MeshDefinition;
 import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.resources.ResourceLocation;
 
-public class HerobrineModel<T extends EntityHerobrine> extends HumanoidModel<T> {
+public class HerobrineModel extends HumanoidModel<EntityHerobrine> {
 
-    public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("herobrinemod", "herobrine"), "main");
+    public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("beyondus", "herobrine"), "main");
 
     public HerobrineModel(ModelPart root) {
         super(root);
     }
 
     public static LayerDefinition createBodyLayer() {
-        MeshDefinition meshdefinition = HumanoidModel.createMesh(CubeDeformation.NONE, 0.0F);
+        MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        // Можно кастомизировать части модели здесь (пока оставим стандартный Humanoid)
+        // Копируем стандартные части HumanoidModel с их размерами и позициями
+        partdefinition.addOrReplaceChild("head",
+            CubeListBuilder.create()
+                .texOffs(0, 0)
+                .addBox(-4.0F, -8.0F, -4.0F, 8, 8, 8),
+            PartPose.offset(0.0F, 0.0F, 0.0F));
+
+        partdefinition.addOrReplaceChild("hat",
+            CubeListBuilder.create(),
+            PartPose.offset(0.0F, 0.0F, 0.0F));
+
+        partdefinition.addOrReplaceChild("body",
+            CubeListBuilder.create()
+                .texOffs(16, 16)
+                .addBox(-4.0F, 0.0F, -2.0F, 8, 12, 4),
+            PartPose.offset(0.0F, 0.0F, 0.0F));
+
+        partdefinition.addOrReplaceChild("right_arm",
+            CubeListBuilder.create()
+                .texOffs(40, 16)
+                .addBox(-3.0F, -2.0F, -2.0F, 4, 12, 4),
+            PartPose.offset(-5.0F, 2.0F, 0.0F));
+
+        partdefinition.addOrReplaceChild("left_arm",
+            CubeListBuilder.create()
+                .texOffs(40, 32)
+                .addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4),
+            PartPose.offset(5.0F, 2.0F, 0.0F));
+
+        partdefinition.addOrReplaceChild("right_leg",
+            CubeListBuilder.create()
+                .texOffs(0, 16)
+                .addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4),
+            PartPose.offset(-1.9F, 12.0F, 0.0F));
+
+        partdefinition.addOrReplaceChild("left_leg",
+            CubeListBuilder.create()
+                .texOffs(0, 32)
+                .addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4),
+            PartPose.offset(1.9F, 12.0F, 0.0F));
 
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
