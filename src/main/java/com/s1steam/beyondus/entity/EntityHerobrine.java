c6a55a8540efc1d4a5dd5555f76bc3a783a15298
S1sTeam <211373039+S1sTeam@users.noreply.github.com>
2025-08-02 02:15:40 +0700

    Э

--- src/main/java/com/s1steam/beyondus/entity/EntityHerobrine.java
+++ src/main/java/com/s1steam/beyondus/entity/EntityHerobrine.java
@@ -26,6 +26,22 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.network.NetworkHooks;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+
 
 
 public class EntityHerobrine extends Monster {
