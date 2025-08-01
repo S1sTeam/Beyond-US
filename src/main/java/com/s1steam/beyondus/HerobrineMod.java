2e801c7af46d1070f2f6280f9574923580c7eb52
S1sTeam <211373039+S1sTeam@users.noreply.github.com>
2025-08-02 02:08:26 +0700

    Update HerobrineMod.java

--- src/main/java/com/s1steam/beyondus/HerobrineMod.java
+++ src/main/java/com/s1steam/beyondus/HerobrineMod.java
@@ -1,21 +1,27 @@
 package com.s1steam.beyondus;
 
 import com.s1steam.beyondus.entity.EntityHerobrine;
+import com.s1steam.beyondus.registry.MySounds;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
-@Mod("beyondus")
+@Mod(HerobrineMod.MODID)
 public class HerobrineMod {
+    public static final String MODID = "beyondus";
 
     public HerobrineMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Регистрируем нашу сущность
+        // Регистрируем сущность
         EntityHerobrine.register(modEventBus);
+
+        // Регистрируем звуки
+        MySounds.register(modEventBus);
 
         // Регистрируем рендер только на клиенте
         if (FMLEnvironment.dist == Dist.CLIENT) {
