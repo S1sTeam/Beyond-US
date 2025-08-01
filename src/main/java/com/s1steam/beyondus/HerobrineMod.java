c82c02497a3f1cd686efe10124afd8746c81325d
S1sTeam <211373039+S1sTeam@users.noreply.github.com>
2025-08-02 01:42:23 +0700

    Update HerobrineMod.java

--- src/main/java/com/s1steam/beyondus/HerobrineMod.java
+++ src/main/java/com/s1steam/beyondus/HerobrineMod.java
@@ -1,32 +1,31 @@
 package com.s1steam.beyondus;
 
 import com.s1steam.beyondus.entity.EntityHerobrine;
-
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(HerobrineMod.MODID)
-@EventBusSubscriber(modid = HerobrineMod.MODID, bus = Bus.MOD, value = Dist.CLIENT)
+@Mod("beyondus")
 public class HerobrineMod {
-    public static final String MODID = "herobrinemod";
 
     public HerobrineMod() {
-        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
-        EntityHerobrine.register(modBus);
-        modBus.addListener(this::clientSetup);
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Регистрируем нашу сущность
+        EntityHerobrine.register(modEventBus);
+
+        // Регистрируем рендер только на клиенте
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            modEventBus.addListener(this::registerEntityRenderers);
+        }
+
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void clientSetup(final FMLClientSetupEvent event) {
-        event.enqueueWork(() -> {
-            EntityRenderersEvent.RegisterRenderers.register(registry -> {
-                registry.registerEntityRenderer(EntityHerobrine.HEROBRINE.get(), EntityHerobrine.Render::new);
-            });
-        });
+    private void registerEntityRenderers(final net.minecraftforge.client.event.EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(EntityHerobrine.HEROBRINE.get(), context -> new EntityHerobrine.Render(context));
     }
 }
