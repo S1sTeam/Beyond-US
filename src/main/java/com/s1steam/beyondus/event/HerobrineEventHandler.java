9e632c9c62be86416f194af9bb39dc6f6026d6a9
S1sTeam <211373039+S1sTeam@users.noreply.github.com>
2025-08-02 01:43:28 +0700

    Update HerobrineEventHandler.java

--- src/main/java/com/s1steam/beyondus/event/HerobrineEventHandler.java
+++ src/main/java/com/s1steam/beyondus/event/HerobrineEventHandler.java
@@ -1,26 +1,22 @@
 package com.s1steam.beyondus.event;
 
-import net.minecraftforge.event.TickEvent.PlayerTickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(modid = "herobrinemod")
 public class HerobrineEventHandler {
 
-    private int ticksPlayed = 0;
-
     @SubscribeEvent
-    public void onPlayerTick(PlayerTickEvent event) {
-        Player player = event.player;
+    public static void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
+        Player player = event.getPlayer();
+        // Пример: отправить игроку сообщение или запустить что-то при входе
         if (!player.level.isClientSide()) {
-            ticksPlayed++;
-
-            // Через каждые 20*60*5 = 6000 тик (5 минут) можно повысить "разум" или вызвать эффект
-            if (ticksPlayed % 6000 == 0) {
-                // Тут можно реализовать логику появления Херобрина или пугалок
-                player.sendSystemMessage(net.minecraft.network.chat.Component.literal("Херобрин становится умнее..."));
-                // Можно запускать звук, спавнить сущность, и т.д.
-            }
+            player.sendSystemMessage(
+                net.minecraft.network.chat.Component.literal("Welcome to the Herobrine mod! Beware..."));
         }
     }
+
+    // Другие события можно добавлять здесь
 }
