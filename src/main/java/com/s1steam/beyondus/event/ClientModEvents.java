7551555d6e5aee470e1ae215db4d28671a673b64
S1sTeam <211373039+S1sTeam@users.noreply.github.com>
2025-08-02 02:40:22 +0700

    Update ClientModEvents.java

--- src/main/java/com/s1steam/beyondus/event/ClientModEvents.java
+++ src/main/java/com/s1steam/beyondus/event/ClientModEvents.java
@@ -1,22 +1,21 @@
-package com.s1steam.beyondus.client;
+package com.s1steam.beyondus;
 
 import com.s1steam.beyondus.entity.EntityHerobrine;
-import com.s1steam.beyondus.entity.HerobrineModel;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = "beyondus", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-public class ClientModEvents {
+@Mod.EventBusSubscriber(modid = HerobrineMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientEventHandler {
 
     @SubscribeEvent
-    public static void onRegisterLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
-        event.registerLayerDefinition(HerobrineModel.LAYER_LOCATION, HerobrineModel::createBodyLayer);
+    public static void onRegisterLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(EntityHerobrine.LAYER_LOCATION, EntityHerobrine::createBodyLayer);
     }
 
     @SubscribeEvent
-    public static void onRegisterEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
-        EntityHerobrine.registerRenderers(event);
+    public static void onRegisterRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(EntityHerobrine.HEROBRINE.get(), EntityHerobrine.Render::new);
     }
 }
