e3548a2cd3680d998ddb6a6e619767fe4663e6bb
S1sTeam <211373039+S1sTeam@users.noreply.github.com>
2025-08-02 17:25:14 +0700

    Delete src/main/java/com/s1steam/beyondus/ClientSetup.java

--- src/main/java/com/s1steam/beyondus/ClientSetup.java
+++
@@ -1,7 +0,0 @@
-@Mod.EventBusSubscriber(modid = "modid", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-public class ClientSetup {
-    @SubscribeEvent
-    public static void onClientSetup(FMLClientSetupEvent event) {
-        EntityRenderers.register(MyEntities.MY_ENTITY.get(), MyEntityRenderer::new);
-    }
-}
